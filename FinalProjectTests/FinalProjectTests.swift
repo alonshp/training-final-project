6796a6e520943d9ffe8ced4e11cf042167173ed1
alonshp <alonshp1@gmail.com>
2018-04-29 17:08:11 +0300

    fix according to oded review

--- FinalProjectTests/FinalProjectTests.swift
+++ FinalProjectTests/FinalProjectTests.swift
@@ -13,23 +13,10 @@ import XCTest
 class FinalProjectTests: XCTestCase {
     
     var weatherWuResponse = [String : Any]()
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testJsonToDictionary() {
         do {
             let testBundle = Bundle(for: type(of: self))
             if let file = testBundle.url(forResource: "wu_response", withExtension: "json") {
@@ -42,33 +29,25 @@ class FinalProjectTests: XCTestCase {
                 }
             } else {
                 print("no file")
-            }
-            if let weatherData = parse() {
-                XCTAssertEqual(weatherData.weatherString , "Clear")
-                XCTAssertEqual(weatherData.temperature , "50.8 F (10.4 C)")
-            } else {
                 XCTFail()
             }
         } catch {
             print(error.localizedDescription)
+            XCTFail()
         }
     }
     
-    func parse() -> WeatherData? {
-        if let currentObservation = weatherWuResponse["current_observation"] as? Dictionary<String,Any>,
-            let weatherString = currentObservation["weather"] as? String,
-            let temperature = currentObservation["temperature_string"] as? String {
-                return WeatherData(weatherString: weatherString, temperature: temperature)
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testJsonToDictionary() {
+        if let weatherData = WeatherData.parseJsonDictionaryToWeatherData(jsonDictionary: weatherWuResponse) {
+            XCTAssertEqual(weatherData.weatherString , "Clear")
+            XCTAssertEqual(weatherData.temperature , "50.8 F (10.4 C)")
         } else {
-            return nil
+            XCTFail()
         }
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
