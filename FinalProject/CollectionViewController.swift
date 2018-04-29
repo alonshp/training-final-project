d9ae6e981f8747302b85d046cbc1438a90990d7f
alonshp <alonshp1@gmail.com>
2018-04-29 09:31:43 +0300

    add collection view controller and collection view cell

---
+++ FinalProject/CollectionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionViewController.swift
+//  FinalProject
+//
+//  Created by Alon Shprung on 4/29/18.
+//  Copyright © 2018 Alon Shprung. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    @IBOutlet var contentView: UIView!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
